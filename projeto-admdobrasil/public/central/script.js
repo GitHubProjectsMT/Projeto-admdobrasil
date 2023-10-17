f5229c87983370f2a484a5d4e329ee4ceee71330
Francisco Neto <netobrgameplay2@gmail.com>
2023-10-17 14:32:14 -0400

    Criação de pastas e páginas em branco.
    Criação de pastas com nome dos integrantes para uso de criação de telas, exemplo telas em branco com css teste

---
+++ projeto-admdobrasil/public/central/script.js
@@ -0,0 +1,39 @@
+const startButton = document.getElementById("start-button");
+      const quitButton = document.getElementById("quit-button");
+      const gameContainer = document.getElementById("game-container");
+      const gameMessage = document.getElementById("game-message");
+      const nextButton = document.getElementById("next-button");
+      const overlay = document.getElementById("overlay");
+      const newPhase = document.getElementById("new-phase");
+      const gameLogo = document.getElementById('game-logo')
+      const body = document.body;
+
+      function iniciarJogo() {
+        startButton.style.display = "none";
+        quitButton.style.display = "block";
+        gameContainer.style.display = "block";
+        gameMessage.innerHTML = "O jogo começou! Divirta-se!";
+      }
+
+      function encerrarJogo() {
+        startButton.style.display = "block";
+        quitButton.style.display = "none";
+        gameContainer.style.display = "none";
+        gameMessage.innerHTML = "Jogo encerrado. Até a próxima!";
+      }
+
+      function proximaFase() {
+        overlay.style.display = "block";
+        newPhase.style.display = "block";
+      }
+
+      function iniciarNovaFase() {
+        overlay.style.display = "none";
+        newPhase.style.display = "none";
+        gameMessage.innerHTML = "Nova fase começou! Boa sorte!";
+    
+
+        // Alterar a imagem de fundo para a nova fase
+        body.style.backgroundImage = "url('../imagens/caminheiro_com_nota_2.png')";
+        gameLogo.style.display = "none"
+      }
